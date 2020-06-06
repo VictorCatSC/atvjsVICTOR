14b925ca7ecd6ba99157bc42f8827cd60df4ffca
Victor Santos <victor01.santos@catolicasc.org.br>
2020-06-05 21:28:05 -0300

    3 arrumar

--- 3.js
+++ 3.js
@@ -4,8 +4,9 @@ var string="";
 whrite(x,y);
 function whrite(x,y){
     for(var j = 1; j <= y; j++){
+        string += j % 2 == 0 ? " " : "";
         for(var i = 1; i <= x; i++){
-            string += "#";
+            string += "# ";
             
         }string += "\n";
         
